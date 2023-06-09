63042c970fff292da0ab687483ec151c5cb7f802
ChumpyTrucker <87389714+ChumpyTrucker@users.noreply.github.com>
2023-06-09 23:45:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/get_phone_number', methods=['POST'])
+def get_phone_number():
+    # Generate and return an IP phone number
+    phone_number = generate_phone_number()
+    return phone_number
+
+def generate_phone_number():
+    # Implement your logic to generate an IP phone number here
+    # This can involve interacting with a service or generating a random number
+    # For simplicity, let's assume the phone number is a random 10-digit number
+    import random
+    phone_number = ''.join(str(random.randint(0, 9)) for _ in range(10))
+    return phone_number
+
+if __name__ == '__main__':
+    app.run(debug=True)
